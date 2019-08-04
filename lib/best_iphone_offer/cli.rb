cd3b2260c77e5aa1d7fb6afc2469aeff5ffa177f
Oana Berentan <oana.berentan@yahoo.com>
2019-08-04 18:50:01 +0000

    tested scrape offers method

--- lib/best_iphone_offer/cli.rb
+++ lib/best_iphone_offer/cli.rb
@@ -12,7 +12,7 @@ class BestIphoneOffer::CLI
     
     
     
-   end
+ 
   
 
   
--- lib/best_iphone_offer/scraper.rb
+++ lib/best_iphone_offer/scraper.rb
@@ -1,10 +1,18 @@
 class BestIphoneOffer::Scraper 
   
+  def self.get_page
+    Nokogiri::HTML(open("https://www.fonehouse.co.uk/brand/apple-mobile-phone-deals"))
+  end
+  
   def self.scrape_offers
-    doc = Nokogiri::HTML(open("https://www.fonehouse.co.uk/brand/apple-mobile-phone-deals"))
-    offers = doc.css('div#filterResults').css('span.handset').text
-    puts offers
-    #binding.pry
+    #handset = get_page.css('div#filterResults').css('span.handset').text.gsub("Apple", "")
+    #puts handset
+    
+    handset = ["iPhone Xr", "iPhone Xs", "iPhone8", "iPhone8", "iPhone7", "iPhone Xs", "iPhone X", "iPhone 6s", "iPhone7", "iPhone 6sPlus"]
+    
+    handset.each_with_index {|h, i| puts "#{i+1}. #{h}"}
+    
+    
  end
 
 end #off the class
