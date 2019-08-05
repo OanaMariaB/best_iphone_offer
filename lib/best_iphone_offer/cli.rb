cc3adbe6d820307ca0120234673ba02b65f67d47
Oana Berentan <oana.berentan@yahoo.com>
2019-08-05 21:01:58 +0000

    update call method

--- lib/best_iphone_offer/cli.rb
+++ lib/best_iphone_offer/cli.rb
@@ -2,15 +2,19 @@ class BestIphoneOffer::CLI
   
   def call
     puts "Welcome! Here are the best iPhone offers at the moment:"
+     list_offer
+     offer_details
+     
+     
+  end #of call
+    
+  def list_offer
      BestIphoneOffer::Scraper.scrape_offers
-     
-     
-      end #of call
-    
-    
-    
- 
+  end
   
-
+  def offer_details
+    puts "Please select from the list which offer are you interested in?"
+  end
+    
   
 end #of class
